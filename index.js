71f1e64bcf6f0fe935a51be6eab32f3e70dc843e
Arnav Bharti <arnavbharti@icloud.com>
2024-05-23 16:28:17 +0530

    modify past notif

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ app.get("/", (req, res) => {
     res.send("Express on Vercel");
 });
 
-notifs = {
-    "notifs": [
-        {
-            "id": 1,
-            "title": "Notif 1",
-            "content": "This is a notification"
-        },
-        {
-            "id": 2,
-            "title": "Notif 2",
-            "content": "This is another notification"
-        }
-    ]
-};
+notifs = [
+    {
+        "id": 1,
+        "title": "Notif 1",
+        "content": "This is a notification"
+    },
+    {
+        "id": 2,
+        "title": "Notif 2",
+        "content": "This is another notification"
+    }
+]
+    ;
 
 app.get("/notifs", (req, res) => {
     res.setHeader('Content-Type', 'application/json');
